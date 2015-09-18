f265e99845a34736256388d47215871a07493000
seokmin_hong <damian2002@naver.com>
2015-09-18 17:22:57 +0000

    Rank.erb complete, Total_Ranking complete

--- app/models/teamlist_lol.rb
+++ app/models/teamlist_lol.rb
@@ -1,3 +1,5 @@
 class TeamlistLol < ActiveRecord::Base
     has_many :fightsubscriptions
+    
+    
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,33 +1,34 @@
+#롤 시드값
 
-TeamlistLol.create(:lol_whichgame => "아하", :lol_team_name => "못난이", :lol_major => "루루", 
+TeamlistLol.create(:lol_whichgame => "아하", :lol_team_name => "못난1", :lol_major => "루루", 
  :lol_current_user_id => "라라",:comment => "히히", :lol_leader_id => "페이커", :lol_user_id_1 => "페이커",
  :lol_user_id_2 => "페이커", :lol_user_id_3 => "페이커" ,:lol_user_id_4 =>"페이커",
 :lol_leader_tear => "브론즈", :lol_tear_user_1 =>"실버", :lol_tear_user_2 =>"골드", :lol_tear_user_3 =>"챌리저", 
 :lol_tear_user_4 =>"마스터", :leader_name => "아라리요", :lol_member_name_1 =>"하하", :lol_member_name_2 =>"재석", :lol_member_name_3 =>"준하",
 :lol_member_name_4 =>"홍철", :lol_total_game => 30 ,:lol_victory => 20,:lol_lose => 3, :lol_winner_point => 10, :lol_winning_rate =>10 )
 
-TeamlistLol.create(:lol_whichgame => "아하", :lol_team_name => "못난이", :lol_major => "루루", 
+TeamlistLol.create(:lol_whichgame => "아하", :lol_team_name => "못난2", :lol_major => "루루", 
  :lol_current_user_id => "라라",:comment => "히히", :lol_leader_id => "페이커", :lol_user_id_1 => "페이커",
  :lol_user_id_2 => "페이커", :lol_user_id_3 => "페이커" ,:lol_user_id_4 =>"페이커",
 :lol_leader_tear => "브론즈", :lol_tear_user_1 =>"실버", :lol_tear_user_2 =>"골드", :lol_tear_user_3 =>"챌리저", 
 :lol_tear_user_4 =>"마스터", :leader_name => "아라리요", :lol_member_name_1 =>"하하", :lol_member_name_2 =>"재석", :lol_member_name_3 =>"준하",
 :lol_member_name_4 =>"홍철", :lol_total_game => 50 ,:lol_victory => 70,:lol_lose => 40, :lol_winner_point => 20, :lol_winning_rate =>50 )
 
-TeamlistLol.create(:lol_whichgame => "아하", :lol_team_name => "못난이", :lol_major => "루루", 
+TeamlistLol.create(:lol_whichgame => "아하", :lol_team_name => "못난3", :lol_major => "루루", 
  :lol_current_user_id => "라라",:comment => "히히", :lol_leader_id => "페이커", :lol_user_id_1 => "페이커",
  :lol_user_id_2 => "페이커", :lol_user_id_3 => "페이커" ,:lol_user_id_4 =>"페이커",
 :lol_leader_tear => "브론즈", :lol_tear_user_1 =>"실버", :lol_tear_user_2 =>"골드", :lol_tear_user_3 =>"챌리저", 
 :lol_tear_user_4 =>"마스터", :leader_name => "아라리요", :lol_member_name_1 =>"하하", :lol_member_name_2 =>"재석", :lol_member_name_3 =>"준하",
 :lol_member_name_4 =>"홍철", :lol_total_game => 70 ,:lol_victory => 30,:lol_lose => 10, :lol_winner_point => 50, :lol_winning_rate =>70 )
 
-TeamlistLol.create(:lol_whichgame => "아하", :lol_team_name => "못난이", :lol_major => "루루", 
+TeamlistLol.create(:lol_whichgame => "아하", :lol_team_name => "못난4", :lol_major => "루루", 
  :lol_current_user_id => "라라",:comment => "히히", :lol_leader_id => "페이커", :lol_user_id_1 => "페이커",
  :lol_user_id_2 => "페이커", :lol_user_id_3 => "페이커" ,:lol_user_id_4 =>"페이커",
 :lol_leader_tear => "브론즈", :lol_tear_user_1 =>"실버", :lol_tear_user_2 =>"골드", :lol_tear_user_3 =>"챌리저", 
 :lol_tear_user_4 =>"마스터", :leader_name => "아라리요", :lol_member_name_1 =>"하하", :lol_member_name_2 =>"재석", :lol_member_name_3 =>"준하",
 :lol_member_name_4 =>"홍철", :lol_total_game => 10 ,:lol_victory => 100,:lol_lose => 20, :lol_winner_point => 40, :lol_winning_rate =>40 )
 
-#롤 시드값
+#피파 시드값
 
 TeamlistFifa.create(:game_id => "aa", :tear => "sdfsdf", :comment => "dsfdsf",
                     :fifa_current_user_id => 1,  :fifa_major => "sdfsdf",
@@ -49,11 +50,11 @@ TeamlistFifa.create(:game_id => "aa", :tear => "sdfsdf", :comment => "dsfdsf",
                     :fifa_user_name =>"거늬",  :fifa_winner_point => 50, :fifa_lose => 7, :fifa_victory => 8,
                     :fifa_winning_rate =>50, :fifa_total_game => 10)
           
-#피파 시드값
+#하스스톤 시드값
 
 TeamlistHearthstone.create(:game_id => "aa", :tear => "sdfsdf", :comment => "dsfdsf",
                     :hearthstone_current_user_id => 1,  :hearthstone_major => "sdfsdf",
-                    :hearthstone_user_name =>"병호",  :hearthstone_winner_point => 20, :hearthstone_lose => 20, :hearthstone_victory => 2,
+                    :hearthstone_user_name =>"홍",  :hearthstone_winner_point => 20, :hearthstone_lose => 20, :hearthstone_victory => 2,
                     :hearthstone_winning_rate =>20, :hearthstone_total_game => 80)
                         
 TeamlistHearthstone.create(:game_id => "aa", :tear => "sdfsdf", :comment => "dsfdsf",
@@ -63,7 +64,7 @@ TeamlistHearthstone.create(:game_id => "aa", :tear => "sdfsdf", :comment => "dsf
                         
 TeamlistHearthstone.create(:game_id => "aa", :tear => "sdfsdf", :comment => "dsfdsf",
                     :hearthstone_current_user_id => 1,  :hearthstone_major => "sdfsdf",
-                    :hearthstone_user_name =>"성중",  :hearthstone_winner_point => 60, :hearthstone_lose => 8, :hearthstone_victory => 7,
+                    :hearthstone_user_name =>"현민",  :hearthstone_winner_point => 60, :hearthstone_lose => 8, :hearthstone_victory => 7,
                     :hearthstone_winning_rate =>60, :hearthstone_total_game => 20)
 
 TeamlistHearthstone.create(:game_id => "aa", :tear => "sdfsdf", :comment => "dsfdsf",
@@ -71,7 +72,83 @@ TeamlistHearthstone.create(:game_id => "aa", :tear => "sdfsdf", :comment => "dsf
                     :hearthstone_user_name =>"거늬",  :hearthstone_winner_point => 50, :hearthstone_lose => 7, :hearthstone_victory => 8,
                     :hearthstone_winning_rate =>50, :hearthstone_total_game => 10)
                     
-#하스스톤 시드값
+TeamlistHearthstone.create(:game_id => "aa", :tear => "sdfsdf", :comment => "dsfdsf",
+                    :hearthstone_current_user_id => 1,  :hearthstone_major => "sdfsdf",
+                    :hearthstone_user_name =>"거늬",  :hearthstone_winner_point => 50, :hearthstone_lose => 7, :hearthstone_victory => 8,
+                    :hearthstone_winning_rate =>50, :hearthstone_total_game => 10)
+                    
+TeamlistHearthstone.create(:game_id => "aa", :tear => "sdfsdf", :comment => "dsfdsf",
+                    :hearthstone_current_user_id => 1,  :hearthstone_major => "sdfsdf",
+                    :hearthstone_user_name =>"거늬",  :hearthstone_winner_point => 50, :hearthstone_lose => 7, :hearthstone_victory => 8,
+                    :hearthstone_winning_rate =>50, :hearthstone_total_game => 10)
+                    
+TeamlistHearthstone.create(:game_id => "aa", :tear => "sdfsdf", :comment => "dsfdsf",
+                    :hearthstone_current_user_id => 1,  :hearthstone_major => "sdfsdf",
+                    :hearthstone_user_name =>"거늬",  :hearthstone_winner_point => 50, :hearthstone_lose => 7, :hearthstone_victory => 8,
+                    :hearthstone_winning_rate =>50, :hearthstone_total_game => 10)
+                    
+TeamlistHearthstone.create(:game_id => "aa", :tear => "sdfsdf", :comment => "dsfdsf",
+                    :hearthstone_current_user_id => 1,  :hearthstone_major => "sdfsdf",
+                    :hearthstone_user_name =>"거늬",  :hearthstone_winner_point => 50, :hearthstone_lose => 7, :hearthstone_victory => 8,
+                    :hearthstone_winning_rate =>50, :hearthstone_total_game => 10)
+                    
+TeamlistHearthstone.create(:game_id => "aa", :tear => "sdfsdf", :comment => "dsfdsf",
+                    :hearthstone_current_user_id => 1,  :hearthstone_major => "sdfsdf",
+                    :hearthstone_user_name =>"거늬",  :hearthstone_winner_point => 50, :hearthstone_lose => 7, :hearthstone_victory => 8,
+                    :hearthstone_winning_rate =>50, :hearthstone_total_game => 10)
+                    
+TeamlistHearthstone.create(:game_id => "aa", :tear => "sdfsdf", :comment => "dsfdsf",
+                    :hearthstone_current_user_id => 1,  :hearthstone_major => "sdfsdf",
+                    :hearthstone_user_name =>"거늬",  :hearthstone_winner_point => 50, :hearthstone_lose => 7, :hearthstone_victory => 8,
+                    :hearthstone_winning_rate =>50, :hearthstone_total_game => 10)
+                    
+TeamlistHearthstone.create(:game_id => "aa", :tear => "sdfsdf", :comment => "dsfdsf",
+                    :hearthstone_current_user_id => 1,  :hearthstone_major => "sdfsdf",
+                    :hearthstone_user_name =>"거늬",  :hearthstone_winner_point => 50, :hearthstone_lose => 7, :hearthstone_victory => 8,
+                    :hearthstone_winning_rate =>50, :hearthstone_total_game => 10)
+                    
+ TeamlistHearthstone.create(:game_id => "aa", :tear => "sdfsdf", :comment => "dsfdsf",
+                    :hearthstone_current_user_id => 1,  :hearthstone_major => "sdfsdf",
+                    :hearthstone_user_name =>"거늬",  :hearthstone_winner_point => 50, :hearthstone_lose => 7, :hearthstone_victory => 8,
+                    :hearthstone_winning_rate =>50, :hearthstone_total_game => 10)
+                    
+TeamlistHearthstone.create(:game_id => "aa", :tear => "sdfsdf", :comment => "dsfdsf",
+                    :hearthstone_current_user_id => 1,  :hearthstone_major => "sdfsdf",
+                    :hearthstone_user_name =>"거늬",  :hearthstone_winner_point => 50, :hearthstone_lose => 7, :hearthstone_victory => 8,
+                    :hearthstone_winning_rate =>50, :hearthstone_total_game => 10)
+                    
+TeamlistHearthstone.create(:game_id => "aa", :tear => "sdfsdf", :comment => "dsfdsf",
+                    :hearthstone_current_user_id => 1,  :hearthstone_major => "sdfsdf",
+                    :hearthstone_user_name =>"거늬",  :hearthstone_winner_point => 50, :hearthstone_lose => 7, :hearthstone_victory => 8,
+                    :hearthstone_winning_rate =>50, :hearthstone_total_game => 10)
+                    
+TeamlistHearthstone.create(:game_id => "aa", :tear => "sdfsdf", :comment => "dsfdsf",
+                    :hearthstone_current_user_id => 1,  :hearthstone_major => "sdfsdf",
+                    :hearthstone_user_name =>"거늬",  :hearthstone_winner_point => 50, :hearthstone_lose => 7, :hearthstone_victory => 8,
+                    :hearthstone_winning_rate =>50, :hearthstone_total_game => 10)
+                    
+TeamlistHearthstone.create(:game_id => "aa", :tear => "sdfsdf", :comment => "dsfdsf",
+                    :hearthstone_current_user_id => 1,  :hearthstone_major => "sdfsdf",
+                    :hearthstone_user_name =>"거늬",  :hearthstone_winner_point => 50, :hearthstone_lose => 7, :hearthstone_victory => 8,
+                    :hearthstone_winning_rate =>50, :hearthstone_total_game => 10)
+                    
+TeamlistHearthstone.create(:game_id => "aa", :tear => "sdfsdf", :comment => "dsfdsf",
+                    :hearthstone_current_user_id => 1,  :hearthstone_major => "sdfsdf",
+                    :hearthstone_user_name =>"거늬",  :hearthstone_winner_point => 50, :hearthstone_lose => 7, :hearthstone_victory => 8,
+                    :hearthstone_winning_rate =>50, :hearthstone_total_game => 10)
+                    
+TeamlistHearthstone.create(:game_id => "aa", :tear => "sdfsdf", :comment => "dsfdsf",
+                    :hearthstone_current_user_id => 1,  :hearthstone_major => "sdfsdf",
+                    :hearthstone_user_name =>"거늬",  :hearthstone_winner_point => 50, :hearthstone_lose => 7, :hearthstone_victory => 8,
+                    :hearthstone_winning_rate =>50, :hearthstone_total_game => 10)
+                    
+TeamlistHearthstone.create(:game_id => "aa", :tear => "sdfsdf", :comment => "dsfdsf",
+                    :hearthstone_current_user_id => 1,  :hearthstone_major => "sdfsdf",
+                    :hearthstone_user_name =>"거늬",  :hearthstone_winner_point => 50, :hearthstone_lose => 7, :hearthstone_victory => 8,
+                    :hearthstone_winning_rate =>50, :hearthstone_total_game => 10)
+                    
+                    
+
 
 AdminUser.create!(email: 'gusals777@naver.com', password: '123123123', password_confirmation: '123123123')  #김현민
 AdminUser.create!(email: 'supurjsgml1@naver.com', password: 'fe239090', password_confirmation: 'fe239090')  #이건희
